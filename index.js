2b9c0db0521aeb47b23684cb6d3d83766eafab9e
Arafat-Rahman <arafat.cuet99@gmail.com>
2023-06-28 12:46:05 +0600

    First Commit

---
+++ index.js
@@ -0,0 +1,78 @@
+var rahim = 400;
+// var karim = 300;
+// var karim = 400;
+var karim = 500;
+
+// console.log(rahim >= karim);
+// console.log(karim <= rahim);
+
+//While Loop
+
+var learningProcess = 0;
+// while(learningProcess < 6){
+//     console.log("I am still learning");
+//     // learningProcess = learningProcess + 1;
+//     // learningProcess += 1;
+//     learningProcess++;
+// }
+
+var number = 1;
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
+
+var number = 2;
+// while(number <= 20){
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// var number = 1;
+// while(number <= 20){
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// for(var number = 1; number <= 20; number = number + 2){
+//     console.log(number);
+// }
+
+// var roastGiven = 0;
+// while(roastGiven <= 100){
+//     console.log("Ami roast peyesi");
+//     roastGiven++;
+// }
+
+// for(var roastGiven = 0; roastGiven <= 100; roastGiven++){
+//     console.log("Ami roast peyesi");
+// }
+
+// for(var index = 1; index<20; index += 2){
+//     console.log(index);
+// }
+
+var number = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+// console.log(number.length);
+// var index = number.indexOf(70);
+// var index = number.indexOf(700);
+// console.log(index);
+// number.push(100);
+// number.unshift(100);
+// number.shift();
+// number.pop();
+// console.log(number);
+
+// for(var i=0; i<number.length; i++){
+//     var element = number[i];
+//     console.log(element);
+// }
+
+var nameOfStudents = ["swapan", "arafat", "tufan", "rhidi", "sabbir"];
+
+// console.log(nameOfStudents);
+for(var i=0; i<nameOfStudents.length; i++){
+    var element = nameOfStudents[i];
+    console.log(element);
+}
